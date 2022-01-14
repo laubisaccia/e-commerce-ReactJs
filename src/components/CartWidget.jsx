6021448351b46f2aac050d0d482a77139144fe54
laubisaccia <mlbisaccia@gmail.com>
2022-01-13 22:52:01 -0300

    FEAT: Adding CartWidget and ItemListContainer comp

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-//import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
